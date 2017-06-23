4859650cc183742e1539e97161526620e0a1aceb
Igor <igorwwwwwwwwwwwwwwwwwwww@users.noreply.github.com>
2017-06-23 17:41:42 +0200

    update name to ex_heroku_client, so that it becomes compatible with mix config

--- lib/ex_heroku_client/builder.ex
+++ lib/ex_heroku_client/builder.ex
@@ -5,5 +5,5 @@ defmodule ExHerokuClient.Builder do
   json = String.replace(
     json, ~s("id": "http://api.heroku.com/schema#"), ~s("id":"#")
   )
-  defapi "Heroku", :heroku, json
+  defapi "Heroku", :ex_heroku_client, json
 end
